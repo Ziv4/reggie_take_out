b4b8f02a38504c2ccb2b0717f51ab65cd9c50854
ziv4 <shiro4ziv@gmail.com>
2023-01-12 23:17:16 +0800

    redis环境配置

---
+++ src/main/java/com/ziv/reggie/config/RedisConfig.java
@@ -0,0 +1,26 @@
+package com.ziv.reggie.config;
+
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * @author ziv
+ * @version 1.0
+ * @className RedisConfig
+ * @date 2023/1/12 23:12:16
+ * @since 1.0
+ */
+@Configuration
+public class RedisConfig extends CachingConfigurerSupport {
+    @Bean
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        return redisTemplate;
+    }
+}
